a88558290f8ee4906bb31dde71e1d90f99be7949
Saad Hussain <saadcfi4@gmail.com>
2025-09-07 14:48:02 +0400

    fix: message bug

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const randomNumberEl = document.querySelector(".js-random-number");
 
 const msgEl = document.querySelector(".js-message");
 
+const errorMsgEl = document.querySelector(".js-error-msg");
+
 // Array used to store numbers that will be generated
-const numbers = [];
+let numbers = [];
 
 /* Testing logic of the code
 
@@ -34,19 +36,33 @@ generateBtn.addEventListener("click", () => {
   const startingInputValue = Number(startingInput.value);
   const endingInputValue = Number(endingInput.value);
 
-  if (!startingInputValue || !endingInputValue) {
-    msgEl.textContent = "Choose 2 valid numbers.";
-    msgEl.style.color = "red";
+  if (startingInput.value.trim() === "" || endingInput.value.trim() === "") {
+    errorMsgEl.textContent = "Choose 2 valid numbers.";
+    errorMsgEl.style.color = "red";
+    return;
   }
 
-  for (let i = startingInputValue + 1; i < endingInputValue; i++) {
+  for (let i = startingInputValue; i <= endingInputValue; i++) {
     // Pushing the numbers being generated into the array
     numbers.push(i);
   }
 
   startingInput.value = "";
   endingInput.value = "";
+
   const randomNumber = numbers[Math.trunc(Math.random() * numbers.length)];
 
   randomNumberEl.textContent = randomNumber;
+
+  /* Clearing the error message on the next clip
+  if user enters 2 valid numbers
+  */
+  errorMsgEl.textContent = "";
+
+  /*
+  Resetting numbers array bc if i dont
+  then itll just keep concating with the
+  old ranges as well
+  */
+  numbers = [];
 });
